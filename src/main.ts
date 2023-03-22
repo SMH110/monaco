ecba1d384986f16a28984faf2560af1ddfd6df4f
Sayed <sayemhasan110@gmail.com>
2023-03-22 16:54:11 +0000

    lunch editor

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
-console.log("Hi")
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+
+
+// Editor placeholder
+const editorHost = document.getElementById("editor");
+
+const editorInstance = monaco.editor.create(editorHost, {
+     value: "let x = 1",
+     theme: "vs-dark",
+     language: "typescript"
+});
+
+
+editorInstance.onDidChangeModelContent(event => console.log("event", event));
+editorInstance.onDidChangeModel(event => {
+    console.log("")
+})
+console.log(monaco)
